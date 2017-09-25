92b1724265157588f8f7cbc750c9e884487a8eaf
ayuspark <lola.luyaowang@gmail.com>
2017-09-25 15:34:23 -0700

    initial commit

---
+++ apps/login_register/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from .forms import *
+
+
+# Create your views here.
+def index(request):
+    form = 
+
+
+
+    return render(request, 'login_register/index.html', context=None)
